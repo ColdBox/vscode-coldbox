45e1901877e56b0d08d6317bee74da6d532bec76
Ilya Verbitskiy <ilya@verbitskiy.co>
2017-11-14 08:38:48 +0100

    added a possibility to disable autocomplete

--- src/extension.ts
+++ src/extension.ts
@@ -7,7 +7,24 @@ const SUPPORTED_LANGUAGES = [
     'cfml'
 ];
 
+function isEnabled(): boolean {
+    let enabled: boolean;
+    let config = vscode.workspace.getConfiguration('coldbox');
+    if (config === null) {
+        enabled = true;
+    }
+    else {
+        enabled = config.get('autocomplete');
+    }
+
+    return enabled;
+}
+
 export function activate(context: vscode.ExtensionContext) {
+    if (!isEnabled()) {
+        return;
+    }
+
     let boxCompletionProvider = new BoxCompletionProvider();
     for (let languageId of SUPPORTED_LANGUAGES) {
         let provider = vscode.languages.registerCompletionItemProvider(
